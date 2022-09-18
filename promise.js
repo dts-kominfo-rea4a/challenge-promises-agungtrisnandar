b63d2462bb3beb30bf6f111bf01ab99cd91fefbf
agungtrisnandar <agung.trisnandar@peruri.co.id>
2022-09-18 11:57:04 +0700

    count data by kondisi

--- promise.js
+++ promise.js
@@ -1,7 +1,24 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (kondisi) => {
+  try {
+    const dataTeaterIXX = await promiseTheaterIXX();
+    const dataTeaterVGC = await promiseTheaterVGC();
+
+    let dataKondisi = 0;
+    
+    dataTeaterIXX.map( item => {
+      if(item.hasil == kondisi) dataKondisi += 1;
+    })
+
+    dataTeaterVGC.map( item => {
+      if(item.hasil == kondisi) dataKondisi += 1;
+    })
+    
+    return dataKondisi;
+  } catch (error) {}
+};
 
 module.exports = {
   promiseOutput,
